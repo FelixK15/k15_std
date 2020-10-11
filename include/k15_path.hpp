7811d583b3614ff1af2a47931037e27898601c5d
Felix <f.klinge15@gmail.com>
2020-10-11 21:19:09 +0200

    add path

---
+++ include/k15_path.hpp
@@ -0,0 +1,15 @@
+#ifndef K15_PATH_INCLUDE
+#define K15_PATH_INCLUDE
+
+#include "k15_std/include/k15_string.hpp"
+
+namespace k15
+{
+    class path
+    {
+      public:
+        path( const string_view& path );
+    };
+}
+
+#endif //K15_PATH_INCLUDE
---
+++ src/k15_path.cpp
@@ -0,0 +1,8 @@
+#include "k15_std/include/k15_path.hpp"
+namespace k15
+{
+    path::path( const string_view& path )
+    {
+        K15_UNUSED_VARIABLE( path );
+    }
+}
